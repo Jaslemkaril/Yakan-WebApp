2d61fe1ec0083a49b92e045b135e3d502660036d
Jaslim <kariljaslem9@gmail.com>
2026-01-20 21:44:26 +0800

    Fix Route facade import in bootstrap

--- routes/setup.php
+++ routes/setup.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
