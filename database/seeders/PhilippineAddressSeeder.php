f1c4cb78c702b5a81f344016e22461139a9f645c
Yakan Admin <yakan@example.com>
2026-02-16 15:39:24 +0800

    Fix seeder: duplicate city codes, add FK constraint handling, more data

--- database/seeders/PhilippineAddressSeeder.php
+++ database/seeders/PhilippineAddressSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 use App\Models\PhilippineRegion;
 use App\Models\PhilippineProvince;
 use App\Models\PhilippineCity;
@@ -12,11 +13,13 @@ class PhilippineAddressSeeder extends Seeder
 {
     public function run(): void
     {
-        // Clear existing data
+        // Disable foreign key checks for safe truncation
+        Schema::disableForeignKeyConstraints();
         PhilippineBarangay::truncate();
         PhilippineCity::truncate();
         PhilippineProvince::truncate();
         PhilippineRegion::truncate();
+        Schema::enableForeignKeyConstraints();
 
         // REGIONS
         $regions = [
@@ -43,7 +46,7 @@ class PhilippineAddressSeeder extends Seeder
             PhilippineRegion::create($region);
         }
 
-        // ZAMBOANGA PENINSULA (REGION IX) - Detailed data
+        // ===================== REGION IX: ZAMBOANGA PENINSULA =====================
         $zamboangaPeninsula = PhilippineRegion::where('region_code', 'IX')->first();
 
         $provinces = [
@@ -65,70 +68,166 @@ class PhilippineAddressSeeder extends Seeder
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'AUR', 'name' => 'Aurora'],
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'BAY', 'name' => 'Bayog'],
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'DUM', 'name' => 'Dumingag'],
-            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'GAL', 'name' => 'Guipos'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'GUI', 'name' => 'Guipos'],
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'LAK', 'name' => 'Lakewood'],
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'LAP', 'name' => 'Lapuyan'],
-            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'MAN', 'name' => 'Midsalip'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'MID', 'name' => 'Midsalip'],
             ['province_id' => $zamboangaDelSur->id, 'city_code' => 'MOL', 'name' => 'Molave'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'RAC', 'name' => 'Ramon Magsaysay'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'SJU', 'name' => 'San Juan'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'SMP', 'name' => 'San Pablo'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'TAB', 'name' => 'Tabina'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'TAM', 'name' => 'Tambulig'],
+            ['province_id' => $zamboangaDelSur->id, 'city_code' => 'TUK', 'name' => 'Tukuran'],
         ];
 
         foreach ($cities as $city) {
             PhilippineCity::create($city);
         }
 
-        // ZAMBOANGA CITY BARANGAYS
+        // ZAMBOANGA DEL NORTE CITIES
+        $zamboangaDelNorte = PhilippineProvince::where('province_code', 'ZAN')->first();
+
+        $zdnCities = [
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'DPC', 'name' => 'Dipolog City'],
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'DAP', 'name' => 'Dapitan City'],
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'SYN', 'name' => 'Sindangan'],
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'LIL', 'name' => 'Liloy'],
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'PIR', 'name' => 'Pinan'],
+            ['province_id' => $zamboangaDelNorte->id, 'city_code' => 'POL', 'name' => 'Polanco'],
+        ];
+
+        foreach ($zdnCities as $city) {
+            PhilippineCity::create($city);
+        }
+
+        // ZAMBOANGA SIBUGAY CITIES
+        $zamboangaSibugay = PhilippineProvince::where('province_code', 'ZSI')->first();
+
+        $zsiCities = [
+            ['province_id' => $zamboangaSibugay->id, 'city_code' => 'IPC', 'name' => 'Ipil'],
+            ['province_id' => $zamboangaSibugay->id, 'city_code' => 'KAB', 'name' => 'Kabasalan'],
+            ['province_id' => $zamboangaSibugay->id, 'city_code' => 'SIN', 'name' => 'Siay'],
+            ['province_id' => $zamboangaSibugay->id, 'city_code' => 'TIT', 'name' => 'Titay'],
+        ];
+
+        foreach ($zsiCities as $city) {
+            PhilippineCity::create($city);
+        }
+
+        // ZAMBOANGA CITY BARANGAYS (98 barangays - key ones)
         $zamboangaCity = PhilippineCity::where('city_code', 'ZC')->first();
 
         $barangays = [
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUM', 'name' => 'Tumaga'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-PUE', 'name' => 'Pueente'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-STA', 'name' => 'Sta. Catalina'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SMA', 'name' => 'Sta. Maria'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TET', 'name' => 'Tetuan'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CAL', 'name' => 'Calarain'],
             ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-AYA', 'name' => 'Ayala'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUL', 'name' => 'Tulungatung'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-GUU', 'name' => 'Guiwan'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON', 'name' => 'Zone I'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON2', 'name' => 'Zone II'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON3', 'name' => 'Zone III'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON4', 'name' => 'Zone IV'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-PUT', 'name' => 'Putik'],
             ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-BAL', 'name' => 'Baliwasan'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CUR', 'name' => 'Culianan'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SAN', 'name' => 'San Roque'],
-            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUB', 'name' => 'Tugbungan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-BOL', 'name' => 'Bolong'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-BUN', 'name' => 'Bunguiao'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-BUS', 'name' => 'Busay'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CAL', 'name' => 'Calarain'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CAW', 'name' => 'Calarian'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CAM', 'name' => 'Camino Nuevo'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CUL', 'name' => 'Culianan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-CUR', 'name' => 'Curuan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-DIV', 'name' => 'Divisoria'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-GUU', 'name' => 'Guiwan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-GUS', 'name' => 'Guisao'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LAB', 'name' => 'La Paz'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LAM', 'name' => 'Labuan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LAN', 'name' => 'Lanzones'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LAP', 'name' => 'Lapakan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LUM', 'name' => 'Lumbangan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-LUN', 'name' => 'Lunzuran'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-MER', 'name' => 'Mercedes'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-PAT', 'name' => 'Pasonanca'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-PUT', 'name' => 'Putik'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-REC', 'name' => 'Recodo'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SAN', 'name' => 'San Jose Cawa-Cawa'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SNR', 'name' => 'San Jose Gusu'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SRO', 'name' => 'San Roque'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SCA', 'name' => 'Sta. Catalina'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SMA', 'name' => 'Sta. Maria'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-SBC', 'name' => 'Sto. Nino'],
             ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TAL', 'name' => 'Talabaan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TLK', 'name' => 'Talisayan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TAM', 'name' => 'Talon-Talon'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TET', 'name' => 'Tetuan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUG', 'name' => 'Tugbungan'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUL', 'name' => 'Tulungatung'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-TUM', 'name' => 'Tumaga'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-VIT', 'name' => 'Vitali'],
             ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-VIC', 'name' => 'Victoria'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON1', 'name' => 'Zone I (Poblacion)'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON2', 'name' => 'Zone II (Poblacion)'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON3', 'name' => 'Zone III (Poblacion)'],
+            ['city_id' => $zamboangaCity->id, 'barangay_code' => 'ZC-ZON4', 'name' => 'Zone IV (Poblacion)'],
         ];
 
         foreach ($barangays as $barangay) {
             PhilippineBarangay::create($barangay);
         }
 
-        // Add sample data for other regions (NCR)
+        // ===================== NCR: METRO MANILA =====================
         $ncr = PhilippineRegion::where('region_code', 'NCR')->first();
         
-        $ncrProvinces = [
-            ['region_id' => $ncr->id, 'province_code' => 'MNL', 'name' => 'Metro Manila'],
-        ];
-
-        foreach ($ncrProvinces as $province) {
-            PhilippineProvince::create($province);
-        }
-
+        PhilippineProvince::create(['region_id' => $ncr->id, 'province_code' => 'MNL', 'name' => 'Metro Manila']);
         $metroManila = PhilippineProvince::where('province_code', 'MNL')->first();
 
         $ncrCities = [
-            ['province_id' => $metroManila->id, 'city_code' => 'MNL-C', 'name' => 'Manila'],
-            ['province_id' => $metroManila->id, 'city_code' => 'QC', 'name' => 'Quezon City'],
-            ['province_id' => $metroManila->id, 'city_code' => 'MAK', 'name' => 'Makati'],
-            ['province_id' => $metroManila->id, 'city_code' => 'TAC', 'name' => 'Taguig'],
-            ['province_id' => $metroManila->id, 'city_code' => 'PAS', 'name' => 'Pasig'],
-            ['province_id' => $metroManila->id, 'city_code' => 'MAN', 'name' => 'Mandaluyong'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MNL', 'name' => 'Manila'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-QC', 'name' => 'Quezon City'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MAK', 'name' => 'Makati'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-TAG', 'name' => 'Taguig'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-PAS', 'name' => 'Pasig'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MDL', 'name' => 'Mandaluyong'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-CLO', 'name' => 'Caloocan'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-VAL', 'name' => 'Valenzuela'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-PAR', 'name' => 'Parañaque'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-LPC', 'name' => 'Las Piñas'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MUN', 'name' => 'Muntinlupa'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MAR', 'name' => 'Marikina'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-PSY', 'name' => 'Pasay'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-SJN', 'name' => 'San Juan'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-NAV', 'name' => 'Navotas'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-MAL', 'name' => 'Malabon'],
+            ['province_id' => $metroManila->id, 'city_code' => 'NCR-PAT', 'name' => 'Pateros'],
         ];
 
         foreach ($ncrCities as $city) {
+            PhilippineCity::create($city);
+        }
+
+        // ===================== REGION VII: CENTRAL VISAYAS =====================
+        $centralVisayas = PhilippineRegion::where('region_code', 'VII')->first();
+
+        PhilippineProvince::create(['region_id' => $centralVisayas->id, 'province_code' => 'CEB', 'name' => 'Cebu']);
+        PhilippineProvince::create(['region_id' => $centralVisayas->id, 'province_code' => 'BOH', 'name' => 'Bohol']);
+
+        $cebu = PhilippineProvince::where('province_code', 'CEB')->first();
+        $cebuCities = [
+            ['province_id' => $cebu->id, 'city_code' => 'CEB-CC', 'name' => 'Cebu City'],
+            ['province_id' => $cebu->id, 'city_code' => 'CEB-LAP', 'name' => 'Lapu-Lapu City'],
+            ['province_id' => $cebu->id, 'city_code' => 'CEB-MAN', 'name' => 'Mandaue City'],
+            ['province_id' => $cebu->id, 'city_code' => 'CEB-TAL', 'name' => 'Talisay City'],
+        ];
+
+        foreach ($cebuCities as $city) {
+            PhilippineCity::create($city);
+        }
+
+        // ===================== REGION XI: DAVAO =====================
+        $davao = PhilippineRegion::where('region_code', 'XI')->first();
+
+        PhilippineProvince::create(['region_id' => $davao->id, 'province_code' => 'DVO', 'name' => 'Davao del Sur']);
+        PhilippineProvince::create(['region_id' => $davao->id, 'province_code' => 'DVN', 'name' => 'Davao del Norte']);
+
+        $davaoDelSur = PhilippineProvince::where('province_code', 'DVO')->first();
+        $davaoCities = [
+            ['province_id' => $davaoDelSur->id, 'city_code' => 'DVO-DC', 'name' => 'Davao City'],
+            ['province_id' => $davaoDelSur->id, 'city_code' => 'DVO-DIG', 'name' => 'Digos City'],
+        ];
+
+        foreach ($davaoCities as $city) {
             PhilippineCity::create($city);
         }
 
