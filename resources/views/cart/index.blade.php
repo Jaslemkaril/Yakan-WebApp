665ccedcf56ef493a773372abe250b2ed14ce868
Heidi Lynn P. Rubia <heidilynnrubia09@gmail.com>
2026-03-03 21:10:25 +0800

    Fix stock display in cart - show proper Out of Stock/Low Stock status

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -378,12 +378,32 @@
                                             </div>
 
                                             <div class="mt-4">
-                                                <span class="stock-badge">
-                                                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
-                                                    </svg>
-                                                    In Stock ({{ $product->stock ?? 0 }} available)
-                                                </span>
+                                                @php
+                                                    $stockLevel = $product->stock ?? 0;
+                                                @endphp
+                                                
+                                                @if($stockLevel > 10)
+                                                    <span class="stock-badge">
+                                                        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
+                                                        </svg>
+                                                        In Stock ({{ $stockLevel }} available)
+                                                    </span>
+                                                @elseif($stockLevel > 0 && $stockLevel <= 10)
+                                                    <span class="inline-flex items-center gap-1.5 px-3 py-1.5 rounded-lg text-sm font-medium bg-yellow-100 text-yellow-800 border border-yellow-300">
+                                                        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
+                                                        </svg>
+                                                        Low Stock ({{ $stockLevel }} left)
+                                                    </span>
+                                                @else
+                                                    <span class="inline-flex items-center gap-1.5 px-3 py-1.5 rounded-lg text-sm font-medium bg-red-100 text-red-800 border border-red-300">
+                                                        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
+                                                        </svg>
+                                                        Out of Stock
+                                                    </span>
+                                                @endif
                                             </div>
                                         </div>
                                     </div>
