9d9dbe0485f956e93fff94f6d457734d6df767b1
Heidi Lynn P. Rubia <heidilynnrubia09@gmail.com>
2026-03-08 10:23:37 +0800

    Fix build failure - use SendGrid via SMTP instead of API

--- config/mail.php
+++ config/mail.php
@@ -49,10 +49,6 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
-        'sendgrid' => [
-            'transport' => 'sendgrid',
-        ],
-
         'ses' => [
             'transport' => 'ses',
         ],
