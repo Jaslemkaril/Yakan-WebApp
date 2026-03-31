57d2830fee0c530a8a26aa38af4b3e69b6e4341d
Heidi Lynn P. Rubia <heidilynnrubia09@gmail.com>
2026-03-31 08:27:46 +0800

    Open address modal from checkout empty state

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -181,7 +181,10 @@
                                     <svg class="w-5 h-5 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
                                     </svg>
-                                    <span>No saved addresses yet. <a href="{{ route('addresses.create') }}" class="font-semibold text-yellow-900 hover:underline">Create one now</a></span>
+                                    <span>
+                                        No saved addresses yet.
+                                        <button type="button" onclick="openNewAddressModal()" class="font-semibold text-yellow-900 hover:underline">Create one now</button>
+                                    </span>
                                 </p>
                             </div>
                         @endif
